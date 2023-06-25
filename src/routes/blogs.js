71ff6cf394eba1507e8967e34af26467281ab711
Tam55si <117524132+Tam55si@users.noreply.github.com>
2023-06-25 12:38:58 +0530

    updated blogs.js

--- src/routes/blogs.js
+++ src/routes/blogs.js
@@ -1,9 +1,7 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import { BlogModel } from '../models/Blogs.js';
-import { UserModel } from "../models/Users.js";
-import { verifyToken } from "./users.js";
-import { useCookies } from 'react-cookie';
+
+
+
 import { getAllBlogs, addBlog, updateBlog, deleteBlog } from '../controllers/blogsController.js';
 
 const router = express.Router();
